3dfe8327c1d8fad96e6002e53e17770f8d491499
meraraee <meramrqz7@gmail.com>
2024-10-30 14:25:50 -0700

    add activity26

---
+++ activity26/activity26.cpp
@@ -0,0 +1,8 @@
+//
+//  activity26.cpp
+//  CIS25
+//
+//  Created by Merary Murillo on 10/30/24.
+//
+
+#include "activity26.hpp"
---
+++ activity26/activity26.hpp
@@ -0,0 +1,13 @@
+//
+//  activity26.hpp
+//  CIS25
+//
+//  Created by Merary Murillo on 10/30/24.
+//
+
+#ifndef activity26_hpp
+#define activity26_hpp
+
+#include <stdio.h>
+
+#endif /* activity26_hpp */
